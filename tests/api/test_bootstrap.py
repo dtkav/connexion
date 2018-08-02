4c3491f449d5387e8c4c269a26c9de79d66b3885
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-08-01 23:36:09 -0700

    switch from swagger_spec_validator to openapi_spec_validator library

--- tests/api/test_bootstrap.py
+++ tests/api/test_bootstrap.py
@@ -2,7 +2,7 @@ import json
 
 import jinja2
 import yaml
-from swagger_spec_validator.common import SwaggerValidationError
+from openapi_spec_validator.exceptions import OpenAPIValidationError
 
 import mock
 import pytest
@@ -205,7 +205,7 @@ def test_add_api_with_function_resolver_function_is_wrapped(simple_api_spec_dir,
 
 def test_default_query_param_does_not_match_defined_type(
         default_param_error_spec_dir):
-    with pytest.raises(SwaggerValidationError):
+    with pytest.raises(OpenAPIValidationError):
         build_app_from_fixture(default_param_error_spec_dir, validate_responses=True, debug=False)
 
 
