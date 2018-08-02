1b9058501c4ea4482cae9d5d4fb5c9173c5f54dc
Daniel Grossmann-Kavanagh <kavanaghdaniel@gmail.com>
2018-08-02 00:08:47 -0700

    default to Swagger2URIParser

--- connexion/operation.py
+++ connexion/operation.py
@@ -10,7 +10,7 @@ from .decorators.response import ResponseValidator
 from .decorators.security import (get_tokeninfo_func, get_tokeninfo_url,
                                   security_passthrough, verify_oauth_local,
                                   verify_oauth_remote)
-from .decorators.uri_parsing import AlwaysMultiURIParser
+from .decorators.uri_parsing import Swagger2URIParser
 from .decorators.validation import ParameterValidator, RequestBodyValidator
 from .exceptions import InvalidSpecification
 from .utils import all_json, is_nullable
@@ -206,7 +206,7 @@ class Swagger2Operation(SecureOperation):
         self.strict_validation = strict_validation
         self.randomize_endpoint = randomize_endpoint
         self.pythonic_params = pythonic_params
-        self.uri_parser_class = uri_parser_class or AlwaysMultiURIParser
+        self.uri_parser_class = uri_parser_class or Swagger2URIParser
         self.pass_context_arg_name = pass_context_arg_name
 
         # todo support definition references
